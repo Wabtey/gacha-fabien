70b8cf6c65816cebacfff00bfcd1e7a877893e6c
Wabtey <73140258+Wabtey@users.noreply.github.com>
2022-03-31 13:13:16 +0200

    restructuration and add simple functionality checker

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let fonc1bool: bool = true;
+    let fonc1: &str = 
+    match fonc1bool {
+        true => "OK",
+        _ => "NOT OK"
+    };
+    println!("GACHA ON");
+    println!("fonctionality One, RUNNNG : {} ", fonc1);
+}
