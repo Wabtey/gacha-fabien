1967bbc95f554e01cc93a71046ec1bba31a50b97
Fepain <florian.epain@etudiant.univ-rennes1.fr>
2022-04-07 14:08:13 +0200

    create roll method : with randomness you can pick into: monkey_stamp or... it's all

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use rand::{thread_rng, Rng};
+mod gacha_bank;
+
+// use gacha_bank::*;
+
+
+fn roll(){
+    let mut rng = thread_rng();
+    let rand_result = rng.gen_range(1..5);
+    let stamp_result =
+        match rand_result {
+            // 1 => gacha_bank::monkey_path,
+            // _ => ""
+            _ => gacha_bank::monkey_path
+        };
+} 
+
+pub fn rolls(amount: i32){
+    let results: vec![&str; amount];
+    for i in 0..amount {
+        results[i]=roll();
+    }
+    results
+}
+
 fn main() {
     let fonc1bool: bool = true;
     let fonc1: &str = 
@@ -6,5 +31,6 @@ fn main() {
         _ => "NOT OK"
     };
     println!("GACHA ON");
+    let results: vec![&str, i32] = rolls(100);
     println!("fonctionality One, RUNNNG : {} ", fonc1);
 }
